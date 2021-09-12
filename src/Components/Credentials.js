6ffceb98646d8d4142c7c5ebf648443155220e5f
CarlaCarrasco <carlacarr37@gmail.com>
2021-09-12 14:48:59 -0400

    Enviorment secrets

--- src/Components/Credentials.js
+++ src/Components/Credentials.js
@@ -1,8 +1,8 @@
 const Credentials = () => {
 
     return {
-        ClientId: 'CLIENT_ID',
-        ClientSecret: 'CLIENT_SECRET'
+        ClientId: CLIENT_ID,
+        ClientSecret: CLIENT_SECRET
     }
 }
 
