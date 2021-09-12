45afe90ee79a38e0d9491f8bf3794408624edb33
CarlaCarrasco <carlacarr37@gmail.com>
2021-09-12 15:24:18 -0400

    change back to original code

--- src/App.js
+++ src/App.js
@@ -13,32 +13,32 @@ function App() {
   const [artistData, setArtistData] = useState([]);
 
   const [token, setToken] = useState('');  
-  // useEffect(() => {
+  useEffect(() => {
 
-  //   axios('https://accounts.spotify.com/api/token', {
-  //     headers: {
-  //       'Content-Type' : 'application/x-www-form-urlencoded',
-  //       'Authorization' : 'Basic ' + btoa(spotify.ClientId + ':' + spotify.ClientSecret)      
-  //     },
-  //     data: 'grant_type=client_credentials',
-  //     method: 'POST'
-  //   })
-  //   .then(tokenResponse => {      
-  //     setToken(tokenResponse.data.access_token);
-  //     axios('https://api.spotify.com/v1/artists?ids=4XquDVA8pkg5Lx91No1JxB%2C75dQReiBOHN37fQgWQrIAJ%2C4mLJ3XfOM5FPjSAWdQ2Jk7%2C6gK1Uct5FEdaUWRWpU4Cl2%2C1fZpYWNWdL5Z3wrDtISFUH%2C69tiO1fG8VWduDl3ji2qhI%2C4C50EbCS11M0VbGyH3OfLt%2C1xLMexpeeTKQ20SwGMaGSK%2C24V5UY0nChKpnb1TBPJhCw%2C3pTE9iaJTkWns3mxpNQlJV%2C44insiIQApkRaCMIbuaISJ%2C4ETSs924pXMzjIeD6E9b4u', {
-  //       method: 'GET',
-  //       headers: { 'Authorization' : 'Bearer ' + tokenResponse.data.access_token}
-  //     }).then(data => {
-  //       setArtistData(data.data.artists)
+    axios('https://accounts.spotify.com/api/token', {
+      headers: {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Authorization' : 'Basic ' + btoa(spotify.ClientId + ':' + spotify.ClientSecret)      
+      },
+      data: 'grant_type=client_credentials',
+      method: 'POST'
+    })
+    .then(tokenResponse => {      
+      setToken(tokenResponse.data.access_token);
+      axios('https://api.spotify.com/v1/artists?ids=4XquDVA8pkg5Lx91No1JxB%2C75dQReiBOHN37fQgWQrIAJ%2C4mLJ3XfOM5FPjSAWdQ2Jk7%2C6gK1Uct5FEdaUWRWpU4Cl2%2C1fZpYWNWdL5Z3wrDtISFUH%2C69tiO1fG8VWduDl3ji2qhI%2C4C50EbCS11M0VbGyH3OfLt%2C1xLMexpeeTKQ20SwGMaGSK%2C24V5UY0nChKpnb1TBPJhCw%2C3pTE9iaJTkWns3mxpNQlJV%2C44insiIQApkRaCMIbuaISJ%2C4ETSs924pXMzjIeD6E9b4u', {
+        method: 'GET',
+        headers: { 'Authorization' : 'Bearer ' + tokenResponse.data.access_token}
+      }).then(data => {
+        setArtistData(data.data.artists)
+
         
-  //     })
-  //   });
+      })
+    });
 
-  // }, [spotify.ClientId, spotify.ClientSecret]); 
+  }, [spotify.ClientId, spotify.ClientSecret]); 
 
-  return<div>Hi</div>
-  console.log(artistData)
- //return artistId ? <Profile selectedArtist={selectedArtist} setArtistId={setArtistId}/> : <Artists selectedArtist={selectedArtist} setSelectedArtist={setSelectedArtist} artistId={artistId} setArtistId={setArtistId} artistData={artistData} setArtistData={setArtistData} />;
+
+  return artistId ? <Profile selectedArtist={selectedArtist} setArtistId={setArtistId}/> : <Artists selectedArtist={selectedArtist} setSelectedArtist={setSelectedArtist} artistId={artistId} setArtistId={setArtistId} artistData={artistData} setArtistData={setArtistData} />;
 }
 
 export default App;
